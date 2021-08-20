05e2a85d855a11f4a61396d1824dd9f30b96da05
TunaSashimi <412504990@qq.com>
2021-08-20 19:12:53 +0800

    feat TunaPasta

--- TunaPasta16/src/main/java/com/tunaPasta16/activity/PathInterpolatorTest.java
+++ TunaPasta16/src/main/java/com/tunaPasta16/activity/PathInterpolatorTest.java
@@ -1,5 +1,6 @@
 package com.tunaPasta16.activity;
 
+import android.animation.ObjectAnimator;
 import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,27 +25,44 @@ public class PathInterpolatorTest extends AppCompatActivity {
         ImageView image_test = findViewById(R.id.image_test);
         image_test.setOnClickListener(v -> {
 
-//            System.out.println("==>"+dpToPx(150 - 30));
 
-//            Animation animation = new TranslateAnimation(0, dpToPx(150 - 30), 0, dpToPx(150 - 30));
-//            Animation animation = new TranslateAnimation(0, 330, 0,330);
-            Animation animation = new TranslateAnimation(0, 660, 0, 0);
-            animation.setDuration(5000);
-            animation.setFillAfter(true);
+
+            ObjectAnimator animation = ObjectAnimator.ofFloat(image_test, "translationX", 100f);
+//            animation.setDuration(2000);
 
             // arcTo() and PathInterpolator only available on API 21+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 Path path = new Path();
-
-                path.moveTo(0, 0);
-
-
-                path.quadTo(1f, 1f, 1, 1);
+                path.arcTo(0f, 0f, 1000f, 1000f, 270f, -180f, true);
                 PathInterpolator pathInterpolator = new PathInterpolator(path);
                 animation.setInterpolator(pathInterpolator);
+                animation.start();
             }
 
-            image_test.startAnimation(animation);
+
+
+
+//            System.out.println("==>"+dpToPx(150 - 30));
+
+//            Animation animation = new TranslateAnimation(0, dpToPx(150 - 30), 0, dpToPx(150 - 30));
+//            Animation animation = new TranslateAnimation(0, 330, 0,330);
+//            Animation animation = new TranslateAnimation(0, 660, 0, 0);
+//            animation.setDuration(5000);
+//            animation.setFillAfter(true);
+//
+//            // arcTo() and PathInterpolator only available on API 21+
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                Path path = new Path();
+//
+//                path.moveTo(0, 0);
+//
+//
+//                path.quadTo(1f, 1f, 1, 1);
+//                PathInterpolator pathInterpolator = new PathInterpolator(path);
+//                animation.setInterpolator(pathInterpolator);
+//            }
+//
+//            image_test.startAnimation(animation);
         });
     }
 
