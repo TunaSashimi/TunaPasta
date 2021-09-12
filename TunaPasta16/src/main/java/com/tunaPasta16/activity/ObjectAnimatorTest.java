c6908a1ce285cc86b2af0cb3b057114f96f71654
jinsheng <jinsheng@didichuxing.com>
2021-09-12 23:29:17 +0800

    feat TunaPasta

--- TunaPasta16/src/main/java/com/tunaPasta16/activity/ObjectAnimatorTest.java
+++ TunaPasta16/src/main/java/com/tunaPasta16/activity/ObjectAnimatorTest.java
@@ -9,7 +9,10 @@ import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -29,8 +32,8 @@ public class ObjectAnimatorTest extends AppCompatActivity {
             {
                     R.drawable.head_man_common,
                     R.drawable.head_woman_common,
-//                    R.drawable.head_boy_common,
-//                    R.drawable.head_boy_common,
+                    R.drawable.head_boy_common,
+                    R.drawable.head_boy_common,
             };
     //
     ImageView image_add;
@@ -67,6 +70,34 @@ public class ObjectAnimatorTest extends AppCompatActivity {
         img_angle_165 = findViewById(R.id.img_angle_165);
         img_angle_105 = findViewById(R.id.img_angle_105);
         img_angle_045 = findViewById(R.id.img_angle_045);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        String[] stringArray = getResources().getStringArray(R.array.flexWrapArray);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, stringArray);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+//                        flexboxLayout.setFlexWrap(FlexWrap.NOWRAP);
+                        break;
+                    case 1:
+//                        flexboxLayout.setFlexWrap(FlexWrap.WRAP);
+                        break;
+                    case 2:
+//                        flexboxLayout.setFlexWrap(FlexWrap.WRAP_REVERSE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         setValue(resourceArray);
     }
